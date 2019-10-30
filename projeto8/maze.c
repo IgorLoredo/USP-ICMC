52ebcb16bd9a963206842099b251c45080a21db7
igor_loredo <igor_loredo@LSEC-055.016645>
2019-10-30 17:50:14 -0300

    todos os projetos sem arquivo explicando como é

--- projeto8/maze.c
+++ projeto8/maze.c
@@ -3,3 +3,14 @@
 #include <maze.h>
 
 
+GRAFO *iniciarGrafo(int ponto,int aberto){
+    GRAFO *new = (GRAFO*)calloc(1,sizeof(GRAFO));
+    new->aberto = aberto;
+    new->ponto = ponto;
+    return new;
+}
+
+pilha_grafo *new_pilha(){
+    pilha_grafo *new = (pilha_grafo*)calloc(1,sizeof(pilha_grafo));
+    return new;
+}
--- projeto8/maze.h
+++ projeto8/maze.h
@@ -2,5 +2,25 @@
 #define maze_h
 
 
+struct grafo{
+    int ponto;
+    int aberto;
+    int visitado;
+    int camara;
+    struct grafo **prox;
+};
+
+struct pilha{
+    struct grato **pilha;
+    int tam;
+};
+
+typedef struct grafo GRAFO;
+typedef struct pilha pilha_grafo;
+
+// pilha e operações
+pilha_grafo *new_pilha();
+
+GRAFO *iniciarGrafo(int,int); // vet,aberto
 
 #endif
